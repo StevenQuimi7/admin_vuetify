c701cb966ffa8a874b2e5d5d54ad4ab760940cb0
Jostin <steven.quimip@ug.edu.ec>
2021-08-12 00:23:30 -0500

    plantilla admin basica v1

---
+++ data-table.js
@@ -0,0 +1,165 @@
+Vue.component('data-table',{
+    template://html
+    `
+
+    <v-row>
+        <v-col>
+            <v-card shaped elevation="8">
+                <v-card-title>Usuarios del Sistema</v-card-title>
+                <v-card-subtitle>Usuario Administrativo</v-card-subtitle>
+                <v-divider></v-divider>             
+                <v-card-text>
+
+                    <v-data-table
+                        dense
+                        :headers="encabezado"
+                        :items="usuarios"
+                        item-key="Nombres"
+                        class="elevation-1"
+                    >
+                        <template v-slot:item.Acciones="{ item }">
+
+                            <v-btn icon color="red">
+                                <v-icon
+                                    small
+                                    class="mr-2"
+                                    
+                                >
+                                    mdi-eye
+                                </v-icon>
+                            </v-btn>
+
+                            <v-btn icon color="red">
+                                <v-icon
+                                    small
+                                    class="mr-2"
+                                    
+                                >
+                                    mdi-pencil
+                                </v-icon>
+                            </v-btn>
+
+                            <v-btn icon color="red">
+                                <v-icon
+                                    small
+                                    
+                                >
+                                    mdi-delete
+                                </v-icon>
+                            </v-btn>
+                        </template>
+                    </v-data-table>
+                
+                </v-card-text>
+        
+            </v-card>
+            
+        <v-col>
+    </row>
+
+    `,
+    data(){
+        return{
+            usuarios: [
+                {
+                  Nombres: 'Steven',
+                  Apellidos: 'Quimi',
+                  Cedula: '0923599890',
+                  Telefono: '0990924825',
+                  Correo: 'stevoperezbsc@hotmail.com',
+                  
+                },
+                {
+                    Nombres: 'Ana',
+                    Apellidos: 'Peralta',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+                {
+                    Nombres: 'Steven',
+                    Apellidos: 'Quimi',
+                    Cedula: '0923599890',
+                    Telefono: '0990924825',
+                    Correo: 'stevoperezbsc@hotmail.com',
+                    
+                },
+              ],
+              encabezado: [
+                {
+                  text: 'Nombres',
+                  align: 'start',
+                  sortable: false,
+                  value: 'Nombres',
+                },
+                
+                { text: 'Apellidos', value: 'Apellidos' },
+                { text: 'Cedula', value: 'Cedula' },
+                { text: 'Telefono', value: 'Telefono' },
+                { text: 'Correo', value: 'Correo' },
+                { text: 'Acciones', value: 'Acciones' },
+                
+              ],
+
+        }
+        
+    }
+    
+})
---
+++ main.js
@@ -0,0 +1,187 @@
+var app= new Vue({
+    el:'#app',
+    data:{
+        name:'steven',
+        drawer:false,
+        izquierda:false,
+        items:[
+            {
+                icon:'mdi-home',
+                text:'Inicio'
+            },
+            {
+                icon:'mdi-view-dashboard',
+                text:'Dashboard'
+            },
+            {
+                icon:'mdi-map',
+                text:'Mapa',
+            },
+            
+        ],
+        items2:[
+            {
+                icon:'mdi-cog',
+                text:'Configuracion',
+            },
+            {
+                icon:'mdi-logout',
+                text:'Cerrar Sesion',
+            },
+        ],
+        usuarios:[
+            {
+                icon:'mdi-account-key',
+                text:'Administrativo',
+            },
+            {
+                icon:'mdi-account-check',
+                text:'Operativo',
+            },
+            {
+                icon:'mdi-account-group',
+                text:'emprendedor',
+            },
+
+        ]
+    },
+    vuetify:new Vuetify(),
+    template://html
+    `
+    <v-app>
+    <!-------------------------NAVIGATION DRAWER-MENU LATERAL------------------------->
+
+        <v-navigation-drawer app color="blue-grey lighten-5" v-model="drawer">
+            
+            <v-list-item>
+                <v-list-item-avatar>
+                    <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item-title>Steven Quimi</v-list-item-title>
+                    <v-list-item-subtitle>stven.quimip@ug.edu.ec</v-list-item-subtitle>
+                </v-list-item-content>
+
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list>
+
+                <v-list-item-group
+                color="gray darken-4"
+                
+                >
+
+                    <v-list-item v-for="item in items" :key="item">
+
+                        <v-list-item-icon>
+                            <v-icon color="indigo lighten-1">{{item.icon}}</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{item.text}}</v-list-item-title>
+                        </v-list-item-content>
+                    
+                    </v-list-item>
+
+                    <v-list-group
+                        :value="false"
+                        prepend-icon="mdi-account"  
+                        color="gray darken-4"
+                 
+                    >
+
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>Usuarios</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+   
+                        <v-list-item v-for="usuario in usuarios" :key="usuario">
+
+                            <v-list-item-icon>
+                                <v-icon color="indigo lighten-1" right>{{usuario.icon}}</v-icon>
+                            </v-list-item-icon>
+
+                            <v-list-item-content>
+                                <v-list-item-title>{{usuario.text}}</v-list-item-title>
+                            </v-list-item-content>
+                        
+                        </v-list-item>
+                        
+                    </v-list-group>
+
+                    <v-list-item v-for="item2 in items2" :key="item2">
+
+                        <v-list-item-icon>
+                            <v-icon color="indigo lighten-1">{{item2.icon}}</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{item2.text}}</v-list-item-title>
+                        </v-list-item-content>
+                    
+                    </v-list-item>
+
+                </v-list-item-group>
+
+            
+            </v-list>
+
+        </v-navigation-drawer>
+
+        <!-----------------------FIN NAVIGATION DRAWER-MENU LATERAL------------------------->
+
+        <!------------------------------------HEADER--------------------------------------->
+
+        <v-app-bar app color="red darken-4" dark>
+    
+            <!-- -->
+            <v-app-bar-nav-icon @click.stop="cambia"></v-app-bar-nav-icon>
+            <v-toolbar-title> Plantilla Admin </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn outlined>Mi Cuenta</v-btn>
+
+        </v-app-bar>
+
+        <!----------------------------------FIN HEADER-------------------------------------->
+    
+        <!---------------------------------CUREPO-BODY-------------------------------------->
+        <v-main>
+            <v-container fluid>
+
+                <data-table>
+                </data-table>
+        
+            </v-container>
+        </v-main>
+        <!-------------------------------FIN CUREPO-BODY-------------------------------------->
+
+
+        <!--------------------------------PIE DE PAGINA------------------------------------->
+    
+        <v-footer color="blue-grey darken-4" dark :class="{izquierda,izquierdaN}">
+                    
+            <v-col
+                class="text-center"
+                cols="12"     
+            >
+                {{ new Date().getFullYear() }} — <strong>stevoperezbsc@hotmail.com</strong>
+            </v-col>
+            
+        </v-footer>
+
+        <!----------------------------- FIN PIE DE PAGINA------------------------------------->
+
+    </v-app>
+
+    `,
+    methods:{
+        cambia(){
+            this.drawer=!this.drawer;
+            this.izquierda=!this.izquierda;
+        }
+    }
+})
